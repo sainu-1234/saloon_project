dcb0e53c108333beae2ffca51c58fbab05946f7d
abdullakvm <abdullakvm10@gmailcom>
2025-11-10 15:28:18 +0530

    responsive code

--- lib/view/otp_screen.dart
+++ lib/view/otp_screen.dart
@@ -8,137 +8,93 @@ class OtpScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        margin: EdgeInsets.all(20),
-        padding: EdgeInsets.all(20),
-        child: Column(
-          spacing: 20,
-          children: [
-            SizedBox(height: 30),
-            Text(
-              "Verify Your Identity",
-              style: TextStyle(
-                color: const Color.fromARGB(255, 4, 4, 2),
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              textAlign: TextAlign.center,
-              "we've sent a 4-digit code to 071*****05\nPlease enter it below",
-              style: TextStyle(
-                color: const Color.fromARGB(255, 138, 138, 127),
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Row(
-              spacing: 27,
-              children: [
-                Container(
-                  height: 60,
-                  width: 60,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFE1DCCC),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "1",
-                      style: TextStyle(
-                        color: const Color.fromARGB(255, 13, 13, 13),
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  height: 60,
-                  width: 60,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFE1DCCC),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "2",
-                      style: TextStyle(
-                        color: const Color.fromARGB(255, 13, 13, 13),
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  height: 60,
-                  width: 60,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFE1DCCC),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "3",
-                      style: TextStyle(
-                        color: const Color.fromARGB(255, 13, 13, 13),
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  height: 60,
-                  width: 60,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFE1DCCC),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "4",
-                      style: TextStyle(
-                        color: const Color.fromARGB(255, 13, 13, 13),
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  textAlign: TextAlign.center,
-                  "Didnt' receive a code?",
-                  style: TextStyle(
-                    color: const Color.fromARGB(255, 138, 138, 127),
-                    fontSize: 17,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Resent",
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment
+                .spaceBetween, //  for place the continue button at the bottom
+            children: [
+              Column(
+                children: [
+                  const SizedBox(height: 30),
+                  const Text(
+                    "Verify Your Identity",
                     style: TextStyle(
-                      color: const Color.fromARGB(255, 2, 60, 248),
-                      fontSize: 17,
+                      color: Color.fromARGB(255, 4, 4, 2),
+                      fontSize: 25,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-              ],
-            ),
-            Container(height: 430),
-            SizedBox(
-              child: SizedBox(
+                  const SizedBox(height: 10),
+                  const Text(
+                    "we've sent a 4-digit code to 071*****05\nPlease enter it below",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(
+                      4,
+                      (index) => Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        height: 60,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(10),
+                          border: index == 3
+                              ? Border.all(color: ColorUtils.blue, width: 2)
+                              : null,
+                        ),
+                        child: Center(
+                          child: Text(
+                            "${index + 1}",
+                            style: const TextStyle(
+                              color: Color.fromARGB(255, 13, 13, 13),
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Didn't receive a code?",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          "Resend",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 2, 60, 248),
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              SizedBox(
                 height: 50,
                 width: double.infinity,
                 child: ElevatedButton(
@@ -146,23 +102,23 @@ class OtpScreen extends StatelessWidget {
                     backgroundColor: WidgetStatePropertyAll(ColorUtils.blue),
                     shape: WidgetStatePropertyAll(
                       RoundedRectangleBorder(
-                        borderRadius: BorderRadiusGeometry.circular(10),
+                        borderRadius: BorderRadius.circular(10),
                       ),
                     ),
                   ),
                   onPressed: () {},
-                  child: Text(
+                  child: const Text(
                     "Continue",
                     style: TextStyle(
-                      color: const Color.fromARGB(255, 245, 247, 250),
+                      color: Color.fromARGB(255, 245, 247, 250),
                       fontSize: 17,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
