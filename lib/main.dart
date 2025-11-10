39131981b664533c745450f5c8aa93c85da94988
sainu-1234 <abhidheensainul@gmail.com>
2025-11-10 22:46:55 +0530

    register page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:saloon_project/view/login_screen.dart';
 import 'package:saloon_project/view/otp_screen.dart';
+import 'package:saloon_project/view/register_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RegisterScreen(),
+    );
   }
 }
 
