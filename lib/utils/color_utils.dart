2dae324344d2298322046a5d82e91291ee4ba459
abdullakvm <abdullakvm10@gmailcom>
2025-11-24 14:07:39 +0530

    tabbar

--- lib/utils/color_utils.dart
+++ lib/utils/color_utils.dart
@@ -1,5 +1,35 @@
-import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
 
 class ColorUtils {
   static const Color blue = Color.fromARGB(255, 1, 54, 178);
+  static List myColorlist = [
+      const Color.fromARGB(255, 136, 32, 32),
+      const Color.fromARGB(255, 8, 221, 118),
+      const Color.fromARGB(255, 7, 85, 158),
+      const Color.fromARGB(255, 128, 11, 192),
+    ];
+}
+class TabBarUtils {
+  static const List<Map<String, dynamic>> myTabs = [
+    {
+      'icon': Icons.content_cut_outlined,
+      'title': 'Hair cut',
+    },
+    {
+      'icon': Icons.face_retouching_natural_outlined,
+      'title': 'Hair Styling',
+    },
+    {
+      'icon': Icons.sentiment_very_satisfied_outlined,
+      'title': 'Shaving',
+    },
+    {
+      'icon': Icons.favorite_border_outlined,
+      'title': 'Nail Styling',
+    },
+    {
+      'icon': Icons.face_2_outlined,
+      'title': ' Face care',
+    },
+  ];
 }
--- lib/view/Home_screen/home_screen.dart
+++ lib/view/Home_screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart'
     show CarouselOptions, CarouselSlider;
 import 'package:flutter/material.dart';
+import 'package:saloon_project/utils/color_utils.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,21 +11,16 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    List mylist = [
-      Colors.black,
-      const Color.fromARGB(255, 136, 32, 32),
-      const Color.fromARGB(255, 8, 221, 118),
-      const Color.fromARGB(255, 7, 85, 158),
-      const Color.fromARGB(255, 128, 11, 192),
-    ];
     return Scaffold(
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 16),
         child: Column(
           spacing: 15,
           children: [
-            SizedBox(height: 20),
             ListTile(
               leading: Icon(Icons.location_on_outlined, color: Colors.blue),
               title: Text("Location", style: TextStyle(fontSize: 20)),
@@ -41,24 +37,24 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: Icon(Icons.notification_important_rounded),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(17),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "Enter address or city name",
-                  prefixIcon: Icon(Icons.search),
-                  filled: true,
-                  fillColor: Colors.grey.shade200,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide.none,
-                  ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Enter address or city name",
+                prefixIcon: Icon(Icons.search),
+                filled: true,
+                fillColor: Colors.grey.shade200,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide.none,
                 ),
               ),
             ),
             CarouselSlider.builder(
               options: CarouselOptions(
-                height: 200,
+                height: 150,
+                pauseAutoPlayOnTouch: true,
+                pauseAutoPlayOnManualNavigate: true,
+                enlargeCenterPage: true,
                 aspectRatio: 10 / 10,
                 viewportFraction: 0.9,
                 initialPage: 0,
@@ -68,98 +64,99 @@ class _HomeScreenState extends State<HomeScreen> {
                 autoPlayInterval: Duration(seconds: 5),
                 autoPlayAnimationDuration: Duration(seconds: 1),
                 autoPlayCurve: Curves.ease,
-                enlargeCenterPage: true,
+
                 enlargeFactor: 0.3,
                 scrollDirection: Axis.horizontal,
               ),
-              itemCount: 5,
+              itemCount: ColorUtils.myColorlist.length,
               itemBuilder:
                   (BuildContext context, int itemIndex, int pageViewIndex) =>
                       Container(
                         decoration: BoxDecoration(
-                          color: mylist[itemIndex],
+                          color: ColorUtils.myColorlist[itemIndex],
                           borderRadius: BorderRadius.circular(10),
                         ),
                       ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 18),
-              child: Row(
-                children: [
-                  Text(
-                    "Services",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-                  ),
-                ],
-              ),
+            Row(
+              children: [
+                Text(
+                  "Services",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 18),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  spacing: 15,
-                  children: List.generate(
-                    5,
-                    (index) => Container(
-                      height: 60,
-                      width: 120,
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 2, 63, 245),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          spacing: 10,
-                          children: [
-                            Icon(
-                              Icons.star_outline_rounded,
-                              size: 26,
-                              color: const Color.fromARGB(255, 248, 243, 243),
-                            ),
-                            Text(
-                              "Hair Cut",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+            // tabbar here
+            _buildTabbar(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Nearby Salons",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: const Color.fromARGB(255, 10, 10, 10),
                   ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 18),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Nearby Salons",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: const Color.fromARGB(255, 10, 10, 10),
-                    ),
+                Text(
+                  "View On Map",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17,
+                    color: const Color.fromARGB(255, 47, 47, 245),
                   ),
-                  Text(
-                    "View On Map",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17,
-                      color: const Color.fromARGB(255, 47, 47, 245),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildTabbar() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(TabBarUtils.myTabs.length, (index) {
+          bool isSelected = currentIndex == index;
+
+          return InkWell(
+            onTap: () {
+            
+              setState(() {
+                currentIndex = index; //  Update selected tab
+              });
+            },
+            child: Container(
+              margin: EdgeInsets.only(right: 8),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              decoration: BoxDecoration(
+                color: isSelected
+                    ? ColorUtils.blue
+                    : Colors.grey.withValues(alpha: 0.2),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    TabBarUtils.myTabs[index]['icon'],
+                    color: isSelected ? Colors.white : ColorUtils.blue,
+                  ),
+                  SizedBox(width: 6),
+                  Text(
+                    TabBarUtils.myTabs[index]['title'],
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: isSelected ? Colors.white : ColorUtils.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }),
       ),
     );
   }
