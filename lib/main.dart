500fcb563165b9a9f1c9332bd1459bd029879c31
sainu-1234 <abhidheensainul@gmail.com>
2025-11-29 11:24:26 +0530

    asdfghjk

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:saloon_project/view/Home_screen/home_screen.dart';
-
+import 'package:saloon_project/view/Saloon_detailes_screen/saloon_detailes_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SaloonDetailesScreen(),
+    );
   }
 }
--- lib/view/Saloon_detailes_screen/saloon_detailes_screen.dart
+++ lib/view/Saloon_detailes_screen/saloon_detailes_screen.dart
@@ -8,6 +8,9 @@ class SaloonDetailesScreen extends StatefulWidget {
 }
 
 class _SaloonDetailesScreenState extends State<SaloonDetailesScreen> {
+  int currentindex = 0;
+  bool isselect = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +38,7 @@ class _SaloonDetailesScreenState extends State<SaloonDetailesScreen> {
         padding: const EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          spacing: 20,
+          spacing: 10,
           children: [
             Container(
               height: 200,
@@ -54,6 +57,80 @@ class _SaloonDetailesScreenState extends State<SaloonDetailesScreen> {
               "Hair Avenue",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                spacing: 2,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                    "Pattambi Road,Perinthalmanna,Malappuram",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    "9AM-10M,Mon - Sun",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    "4.7 (321)",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              textAlign: TextAlign.justify,
+              "Hair Avenue provides expert haircuts,syling,along with servieces like facials,cleanups,skincare and make to keep you looking your best.",
+              overflow: TextOverflow.ellipsis,
+              maxLines: 5,
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+            ),
+
+            SizedBox(height: 20),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(5, (index) {
+                  isselect = currentindex == index;
+                  return Padding(
+                    padding: EdgeInsetsGeometry.symmetric(horizontal: 15),
+                    child: InkWell(
+                      onTap: () {
+                        currentindex = index;
+                        setState(() {});
+                      },
+                      child: Text(
+                        "sainu",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: isselect
+                              ? const Color.fromARGB(255, 5, 92, 190)
+                              : Colors.black,
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
+            // ListView.builder(
+            //   shrinkWrap: true,
+            //   physics: NeverScrollableScrollPhysics(),
+            //   itemCount: 4,
+            //   itemBuilder: (context, index) {
+            //     return Container(
+            //       margin: EdgeInsets.symmetric(vertical: 10),
+            //       child: ListTile(
+            //         title: Text("Hair Cut"),
+            //         subtitle: Text("\$300 30 Mins"),
+            //         trailing: Icon(Icons.add_circle_outline),
+            //       ),
+            //     );
+            //   },
+            // ),
           ],
         ),
       ),
